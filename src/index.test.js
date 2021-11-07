ffc4485e0d5ea24357f826faa9a9b0577f47e8fc
Terr1bus <qwant94@mail.ru>
2021-11-07 16:44:18 +0300

    test: break test

--- src/index.test.js
+++ src/index.test.js
@@ -2,6 +2,6 @@ const { add } = require('./index');
 
 describe('add spec', () => {
   test('should summ two passed numbers', () => {
-    expect(add(1, 2)).toBe(3);
+    expect(add(1, 2)).toBe(4);
   });
 });
