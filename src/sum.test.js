ac3110793d848cb30e259541d0f28ace4ff289c7
Terr1bus <qwant94@mail.ru>
2021-11-07 21:40:12 +0300

    test: fix test

--- src/sum.test.js
+++ src/sum.test.js
@@ -2,6 +2,6 @@ const { add } = require('./sum');
 
 describe('add spec', () => {
   test('should summ two passed numbers', () => {
-    expect(add(1, 2)).toBe(4);
+    expect(add(1, 2)).toBe(3);
   });
 });
